731735b5423b28e2798fe7ded452fb421e78ec24
jorgeantonio21 <matroid@outlook.com>
2023-09-27 09:22:46 +0100

    code clean and bug refactor

--- echo/src/main.rs
+++ echo/src/main.rs
@@ -7,16 +7,16 @@ use std::io::{self};
 
 fn main() -> Result<(), Error> {
     let stdin = io::stdin();
+    let inputs = serde_json::Deserializer::from_reader(stdin.lock()).into_iter::<Message>();
+
     let stdout = io::stdout();
-    let stdout_lock = stdout.lock();
-    let mut output = serde_json::Serializer::new(stdout_lock);
+    let mut stdout_lock = stdout.lock();
 
     let mut echo_node = EchoNode::new();
 
-    let inputs = serde_json::Deserializer::from_reader(stdin.lock()).into_iter::<Message>();
     for input in inputs {
         let message = input.context("Failed to deserialize STDIN input message")?;
-        echo_node.step(message, &mut output)?;
+        echo_node.step(message, &mut stdout_lock)?;
     }
 
     Ok(())
--- echo/src/node.rs
+++ echo/src/node.rs
@@ -1,14 +1,10 @@
-use crate::{body::Body, message::Message, r#type::Type};
-use anyhow::{Context, Error};
-use serde::Serialize;
 use std::io::StdoutLock;
 
+use crate::{body::Body, message::Message, r#type::Type, serialize_to_stdout};
+use anyhow::{bail, Error};
+
 pub trait Node {
-    fn step(
-        &mut self,
-        message: Message,
-        output: &mut serde_json::Serializer<StdoutLock>,
-    ) -> Result<(), Error>;
+    fn step(&mut self, message: Message, stdout_lock: &mut StdoutLock) -> Result<(), Error>;
 }
 
 pub struct EchoNode {
@@ -22,26 +18,36 @@ impl EchoNode {
 }
 
 impl Node for EchoNode {
-    fn step(
-        &mut self,
-        message: Message,
-        output: &mut serde_json::Serializer<StdoutLock>,
-    ) -> Result<(), Error> {
+    fn step(&mut self, message: Message, stdout_lock: &mut StdoutLock) -> Result<(), Error> {
         match message.body.r#type {
+            Type::Init { .. } => {
+                let reply = Message {
+                    dest: message.src,
+                    src: message.dest,
+                    body: Body {
+                        msg_id: Some(self.id),
+                        in_reply_to: message.body.msg_id,
+                        r#type: crate::r#type::Type::InitOk,
+                    },
+                };
+                serialize_to_stdout(&reply, stdout_lock)?;
+                self.id += 1;
+            }
+            Type::InitOk => {
+                bail!("Unexpected InitOk message")
+            }
             Type::EchoOk { .. } => {}
             Type::Echo { echo } => {
                 let reply = Message {
-                    dest: message.src.clone(),
-                    src: message.dest.clone(),
+                    dest: message.src,
+                    src: message.dest,
                     body: Body {
                         msg_id: Some(self.id),
                         in_reply_to: message.body.msg_id,
                         r#type: crate::r#type::Type::EchoOk { echo: echo.clone() },
                     },
                 };
-                reply
-                    .serialize(output)
-                    .context("Failed to serialize EchoOk response.")?;
+                serialize_to_stdout(&reply, stdout_lock)?;
                 self.id += 1;
             }
         };
